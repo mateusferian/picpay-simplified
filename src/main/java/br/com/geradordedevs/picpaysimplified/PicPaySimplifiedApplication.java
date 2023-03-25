ded48681e080cd2fab96a1f27c01f73f8fa376c9
mateusferian <mateusferian10@gmail.com>
2023-03-25 09:40:47 -0300

    :tada: initial commit

---
+++ src/main/java/br/com/geradordedevs/picpaysimplified/PicPaySimplifiedApplication.java
@@ -0,0 +1,13 @@
+package br.com.geradordedevs.picpaysimplified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPaySimplifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPaySimplifiedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/geradordedevs/picpaysimplified/PicPaySimplifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geradordedevs.picpaysimplified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPaySimplifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
