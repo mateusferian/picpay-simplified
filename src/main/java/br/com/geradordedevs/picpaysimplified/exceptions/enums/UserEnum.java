41af34f81d12045ce8abbc595adf2f0df8043361
mateusferian <mateusferian10@gmail.com>
2023-03-30 10:53:36 -0300

    :recycle: refactor: passing message to english in UserEnum

--- src/main/java/br/com/geradordedevs/picpaysimplified/exceptions/enums/UserEnum.java
+++ src/main/java/br/com/geradordedevs/picpaysimplified/exceptions/enums/UserEnum.java
@@ -9,13 +9,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public enum UserEnum{
 
-    THIS_EMAIL_IS_ALREADY_BEING_USED("THIS_EMAIL_IS_ALREADY_BEING_USED", "Este email já esta sendo usado",400),
+    THIS_EMAIL_IS_ALREADY_BEING_USED("THIS_EMAIL_IS_ALREADY_BEING_USED", "This email is already being used",400),
 
-    THIS_EMAIL_AND_CPF_IS_ALREADY_BEING_USED("THIS_EMAIL_AND_CPF_IS_ALREADY_BEING_USED", "Este email e numero de documento já estão sendo usados",400),
+    THIS_EMAIL_AND_CPF_IS_ALREADY_BEING_USED("THIS_EMAIL_AND_CPF_IS_ALREADY_BEING_USED", "This email and document number are already in use",400),
 
-    THIS_CPF_IS_ALREADY_BEING_USED("THIS_CPF_IS_ALREADY_BEING_USED", "Este numero de documento já esta sendo usado",400),
+    THIS_CPF_IS_ALREADY_BEING_USED("THIS_CPF_IS_ALREADY_BEING_USED", "This document number is already being used",400),
 
-    INVALID_USERNAME_OR_PASSWORD("INVALID_USERNAME_OR_PASSWORD", "Usuario ou senha invalidos",400);
+    INVALID_USERNAME_OR_PASSWORD("INVALID_USERNAME_OR_PASSWORD", "Username or password is invalid",400);
 
     private String code;
     private String message;
