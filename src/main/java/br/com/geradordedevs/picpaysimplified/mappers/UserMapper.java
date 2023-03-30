c3461ff906e0115d4271521b547ee55285f77be1
mateusferian <mateusferian10@gmail.com>
2023-03-29 23:23:33 -0300

    :sparkles: feat: creating the mappers package

---
+++ src/main/java/br/com/geradordedevs/picpaysimplified/mappers/UserMapper.java
@@ -0,0 +1,42 @@
+package br.com.geradordedevs.picpaysimplified.mappers;
+
+import br.com.geradordedevs.picpaysimplified.dtos.responses.UserResponseDTO;
+import br.com.geradordedevs.picpaysimplified.dtos.requests.UserRequestDTO;
+import br.com.geradordedevs.picpaysimplified.entities.UserEntity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class UserMapper {
+
+    @Autowired
+    private ModelMapper mapper;
+
+    public UserEntity toEntity(UserRequestDTO userRequestDTO){
+        log.info("converting dto{} to entity", userRequestDTO);
+        return mapper.map(userRequestDTO,UserEntity.class);
+    }
+
+    public UserResponseDTO toDto(UserEntity userEntity){
+        log.info("converting entity{} to dto", userEntity);
+        return mapper.map(userEntity,UserResponseDTO.class);
+    }
+
+    public List<UserResponseDTO> toDtoList(Iterable<UserEntity> userEntity){
+        log.info("converting entity list{} to dto list", userEntity);
+        List<UserEntity> userEntities = new ArrayList<>();
+        userEntity.forEach(userEntities::add);
+        return userEntities.stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
+    }
+}
