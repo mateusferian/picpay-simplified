17135d9f675b9f0d2caefe3391337f62807a4114
mateusferian <mateusferian10@gmail.com>
2023-03-30 10:49:57 -0300

    :recycle: refactor: passing message to english in TransferEnum

--- src/main/java/br/com/geradordedevs/picpaysimplified/exceptions/enums/TransferEnum.java
+++ src/main/java/br/com/geradordedevs/picpaysimplified/exceptions/enums/TransferEnum.java
@@ -10,16 +10,15 @@ import lombok.NoArgsConstructor;
 public enum TransferEnum {
 
     INCORRECT_DOCUMENT_NUMBER("INCORRECT_DOCUMENT_NUMBER",
-            "trasferencia cancelada,numero do documento do destinatario ou do usuario que efetuou a trasferencia incorreto",400),
+            "canceled transfer, incorrect document number of the recipient or user who performed the transfer",400),
 
-    NEGATIVE_BALANCE("NEGATIVE_BALANCE","trasferencia cancelada,saldo negativo ou igual a zero",404),
+    NEGATIVE_BALANCE("NEGATIVE_BALANCE","transfer cancelled, balance negative or equal to zero",404),
 
-    INVALID_DOCUMENT_NUMBER_OR_PASSWORD("INVALID_DOCUMENT_NUMBER_OR_PASSWORD", "trasferencia cancelada," +
-            "numero de documento ou senha invalidos",400),
+    INVALID_DOCUMENT_NUMBER_OR_PASSWORD("INVALID_DOCUMENT_NUMBER_OR_PASSWORD", "invalid document number or password",400),
 
-    DOCUMENT_NUMBER_NOT_FOUND("DOCUMENT_NUMBER_NOT_FOUND", "numero do documento não encontrado",404),
+    DOCUMENT_NUMBER_NOT_FOUND("DOCUMENT_NUMBER_NOT_FOUND", "document number not found",404),
 
-    INCORRECT_USER_TYPE("INCORRECT_USER_TYPE", "tipo de usuario inpossibilitado de efetuar trasferencia",400);
+    INCORRECT_USER_TYPE("INCORRECT_USER_TYPE", "this type of user cannot transfer",400);
 
     private String code;
     private String message;
