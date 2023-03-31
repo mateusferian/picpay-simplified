47173a313e4ad3770174ca3f90d466bfb1afb6d6
mateusferian <mateusferian10@gmail.com>
2023-03-31 08:32:40 -0300

    :recycle: refactor: adjusting the RequestMapping that is in TransferController

--- src/main/java/br/com/geradordedevs/picpaysimplified/controllers/TransferController.java
+++ src/main/java/br/com/geradordedevs/picpaysimplified/controllers/TransferController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/transfer")
+@RequestMapping("/api/v1/transfers")
 public class TransferController {
 
     @Autowired
