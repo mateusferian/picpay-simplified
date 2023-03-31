ec65aa9645e19281b4621eadeda812562f2fc654
mateusferian <mateusferian10@gmail.com>
2023-03-31 10:55:45 -0300

    :recycle: refactor: refactoring UserEnum

--- src/main/java/br/com/geradordedevs/picpaysimplified/exceptions/enums/UserEnum.java
+++ src/main/java/br/com/geradordedevs/picpaysimplified/exceptions/enums/UserEnum.java
@@ -11,9 +11,9 @@ public enum UserEnum{
 
     THIS_EMAIL_IS_ALREADY_BEING_USED("THIS_EMAIL_IS_ALREADY_BEING_USED", "This email is already being used",400),
 
-    THIS_EMAIL_AND_CPF_IS_ALREADY_BEING_USED("THIS_EMAIL_AND_CPF_IS_ALREADY_BEING_USED", "This email and document number are already in use",400),
+    DOCUMENT_NUMBER_AND_EXISTING_EMAIL("DOCUMENT_NUMBER_AND_EXISTING_EMAIL", "This email and document number are already in use",400),
 
-    THIS_CPF_IS_ALREADY_BEING_USED("THIS_CPF_IS_ALREADY_BEING_USED", "This document number is already being used",400),
+    DOCUMENT_NUMBER_EXISTING("DOCUMENT_NUMBER_EXISTING", "This document number is already being used",400),
 
     MANDATORY_PASSWORD("MANDATORY_PASSWORD", "Required password",401);
 
