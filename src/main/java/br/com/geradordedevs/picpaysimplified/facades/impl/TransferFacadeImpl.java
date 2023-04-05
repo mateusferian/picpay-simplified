45ded082e1e753c0de52dd76b0b86c9fbe65eb02
mateusferian <mateusferian10@gmail.com>
2023-04-05 10:50:35 -0300

    :recycle: refactor: simplifying the method of TransferFacadeImpl

--- src/main/java/br/com/geradordedevs/picpaysimplified/facades/impl/TransferFacadeImpl.java
+++ src/main/java/br/com/geradordedevs/picpaysimplified/facades/impl/TransferFacadeImpl.java
@@ -7,7 +7,6 @@ import br.com.geradordedevs.picpaysimplified.exceptions.TransferException;
 import br.com.geradordedevs.picpaysimplified.exceptions.enums.TransferEnum;
 import br.com.geradordedevs.picpaysimplified.facades.TransferFacade;
 import br.com.geradordedevs.picpaysimplified.services.TransferService;
-import br.com.geradordedevs.picpaysimplified.services.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,24 +16,21 @@ import org.springframework.stereotype.Component;
 public class TransferFacadeImpl implements TransferFacade {
 
     @Autowired
-    private UserService userService;
-
-    @Autowired
     private TransferService transferService;
 
     @Override
     public TransferResponseDTO transfer(TransferRequestDTO transferRequestDTO) {
 
-        if (userService.findById(transferRequestDTO.getPayer()).getTypeOfUser() == TypeOfUser.SHOPKEEPER){
+        if (transferService.findByIdPayer(transferRequestDTO.getPayer()).getTypeOfUser() == TypeOfUser.SHOPKEEPER){
 
             throw new TransferException(TransferEnum.INCORRECT_USER_TYPE);
 
-        } else if (userService.findById(transferRequestDTO.getPayer()).getValue().doubleValue() <= 0) {
+        } else if (transferService.findByIdPayer(transferRequestDTO.getPayer()).getValue().doubleValue() <= 0) {
 
             throw new TransferException(TransferEnum.NEGATIVE_BALANCE);
 
         } else {
-            userService.validateUserPassword(transferRequestDTO.getPassword(),transferRequestDTO.getPayer());
+            transferService.validatePayerPassword(transferRequestDTO.getPassword(),transferRequestDTO.getPayer());
             return transferService.transfer(transferRequestDTO);
         }
     }
